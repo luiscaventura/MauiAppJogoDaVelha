429e511a652af97f0b2b1eb124ac9d06e2d487c9
User <User@BP20230309>
2024-10-14 10:34:10 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppJogoDaVelha/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿namespace MauiAppJogoDaVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        string vez = "X";
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            btn.IsEnabled = false;
+
+            if (vez == "X")
+
+            {
+                btn.Text = "X";
+                vez = "O";
+            }
+            else
+            {
+                btn.Text = "O";
+                vez = "X";
+            }
+
+            /* Verificando se o X ganhou na 1ª linha */
+
+            if (btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X")
+            {
+                DisplayAlert("Parabéns", "O X ganhou!", "OK");
+                Zerar();
+            }
+
+        } //Fecha método
+
+        void Zerar()
+        {
+            btn10.Text = "";
+            btn11.Text = "";
+            btn12.Text = "";
+            btn20.Text = "";
+            btn21.Text = "";
+            btn22.Text = "";
+            btn30.Text = "";
+            btn31.Text = "";
+            btn32.Text = "";
+
+            btn10.IsEnabled = true;
+            btn11.IsEnabled = true;
+            btn12.IsEnabled = true;
+            btn20.IsEnabled = true;
+            btn21.IsEnabled = true;
+            btn22.IsEnabled = true;
+            btn30.IsEnabled = true;
+            btn31.IsEnabled = true;
+            btn32.IsEnabled = true;
+
+        }
+
+        
+    } //Fecha Classe
+
+} //Fecha namespace
